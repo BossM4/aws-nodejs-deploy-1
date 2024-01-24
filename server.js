7867d9b9fb497dcf984f5c00dca3cc39f685bc05
Nattapong <nattapong.vega@gmail.com>
2024-01-24 19:11:13 +0700

    get user details

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/api/get', (req,res)=> {
     res.send({message:"Hello Backend Node JS"})
 })
 
+app.get('/api/get_user_details'(req,res)=> {
+    res.send({
+        user:{
+            name:"Nattapong",
+            Position:'BOSS',
+            contact:'0987654321'
+        }
+    })
+})
+
 app.listen(port, ()=> {
     console.log("Server running on port",port)
 })
