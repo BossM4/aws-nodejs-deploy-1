4fdc9cbed69ddb7217f186767ba9ae320d7567c8
Nattapong <nattapong.vega@gmail.com>
2024-01-25 09:44:24 +0700

    add user details -age

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT =8000
-// require("dotenv").config
 
 app.get('/api/get', (req,res)=> {
     res.send({message:"Hello Backend Node JS"})
@@ -16,11 +15,6 @@ app.get('/api/register', (req,res)=> {
     res.send({message:"This is Registrer pages"})
 })
 
-<<<<<<< HEAD
-app.listen(PORT, ()=> {
-    console.log("Server running on port ",PORT)
-})
-=======
 // time clock
 app.get('/api/time', (req,res)=> {
     res.send({message:"This is TIME Clock pages"})
@@ -29,4 +23,3 @@ app.get('/api/time', (req,res)=> {
 app.listen(port, ()=> {
     console.log("Server running on port",port)
 })
->>>>>>> 062a7174381fec86adcf6ee04c5ebb42601776fe
